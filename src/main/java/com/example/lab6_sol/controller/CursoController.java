c58bce468183118a2cb5d439ed3c0690fb65a2db
adrian <adrian.lopez@pucp.edu.pe>
2023-05-23 21:53:06 -0500

    "commit1"

--- src/main/java/com/example/lab6_sol/controller/CursoController.java
+++ src/main/java/com/example/lab6_sol/controller/CursoController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -19,10 +21,16 @@ public class CursoController {
     @Autowired
     CursoRepository cursoRepository;
 
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
     @GetMapping("/lista")
     public String listaCurso(Model model){
         List<Curso> cursos = cursoRepository.findAll();
         model.addAttribute("cursos", cursos);
         return "lista_cursos";
     }
+
+
+
 }
--- src/main/java/com/example/lab6_sol/controller/EstudianteController.java
+++ src/main/java/com/example/lab6_sol/controller/EstudianteController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -22,5 +24,26 @@ public class EstudianteController {
         List<Usuario> estudiantes = usuarioRepository.findByRolid(5);
         model.addAttribute("estudiantes", estudiantes);
         return "lista_usuarios";
+    }
+
+    @GetMapping("/nuevoEstudiante")
+    public String nuevoEstudiante(Model model){
+//        List<Curso> cursos = cursoRepository.findAll();
+
+        Usuario usuario = new Usuario();
+
+        model.addAttribute("usuario", usuario);
+        return "nuevo_estudiante";
+    }
+
+    @PostMapping("/save")
+    public String guardarNuevoEstudiante(Usuario usuario, RedirectAttributes attr) {
+        if (usuario.getId() == 0) {
+            attr.addFlashAttribute("msg", "Usuario creado exitosamente");
+        } else {
+            attr.addFlashAttribute("msg", "Usuario actualizado exitosamente");
+        }
+        usuarioRepository.save(usuario);
+        return "redirect:/estudiante/lista";
     }
 }
